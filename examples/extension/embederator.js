9a4149df08389a4549addbb13564f77cdd2cb27b
Rupert Redington <rupert@neontribe.co.uk>
2015-11-20 14:49:30 +0000

    Path widgets to production

--- examples/extension/embederator.js
+++ examples/extension/embederator.js
@@ -2,6 +2,6 @@
 	var target = $('h2:eq(4)');
 	var script = document.createElement( 'script' );
 	script.type = 'text/javascript';
-	script.src = 'http://localhost:3000/javascripts/sabot.js?spaceId=rvh';
+	script.src = 'http://helpmargery.herokuapp.com/javascripts/sabot.js?spaceId=rvh';
 	target.before(script);
 })();
