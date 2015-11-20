45bbcf2e026376daa69190a845b385bfcef50a07
Stansbridge <github@anthonystansbridge.co.uk>
2015-11-20 12:19:46 +0000

    Added base views for creating a new space.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Index */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Help Margery' });
+  res.render('index', { 
+	  title: 'Help Margery',
+  });
+});
+
+/* Space */
+router.get('/space', function(req, res, next) {
+	res.render('space/create', { 
+		title: 'Create a new Space',
+	});
+});
+router.post('/space', function(req, res, next) {
+	res.render('space/success', {
+		title: "Success, new Space created",
+		post: req.body,
+	});	
 });
 
 module.exports = router;
