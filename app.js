fc387c408552cfe217f2d900788e360e1d20eee9
Rupert Redington <rupert@neontribe.co.uk>
2015-11-20 10:33:51 +0000

    clean up

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
