c5dd6010197d538fb2e63846ae0300b95d618a23
Stansbridge <github@anthonystansbridge.co.uk>
2015-11-20 16:52:50 +0000

    Injected lodash into ejs templates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var routes = require('./routes/index');
 var widget = require('./routes/widget');
@@ -14,6 +15,8 @@ var app = express();
 app.engine('ejs', require('ejs-mate'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// inject lodash/underscore into ejs
+app.locals._ = _;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
