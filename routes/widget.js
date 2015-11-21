6814c8cea1a3523e0e14f42bfde5abff9d903f8f
Robert Preus-MacLaren <rpprroger@googlemail.com>
2015-11-21 00:34:22 +0000

    Fixed some style leakage

--- routes/widget.js
+++ routes/widget.js
@@ -3,12 +3,15 @@ var router = express.Router();
 
 var models = require('../models');
 
-
 router.get('/sabot.js', function(req, res, next){
 	res.set('Content-Type', 'text/javascript');
 	var spaceId = req.query.id;
 	models.Space.findOne({_id: spaceId}, function(err, space) {
+		if (!space) {
+			err = 'Space missing space';
+		}
 		if (err) {
+			console.log(err);
 			res.send('');
 		} else {
 			res.render('sabot', {spaceId: spaceId});
@@ -19,6 +22,9 @@ router.get('/sabot.js', function(req, res, next){
 
 router.get('/:id', function(req, res, next) {
 	models.Space.findOne({_id: req.params.id}, function(err, space) {
+		if (!space) {
+			err = 'Space missing';
+		}
 		if (err) {
 			console.log(err);
 			next(err);
@@ -33,6 +39,9 @@ router.get('/:id', function(req, res, next) {
 
 router.post('/:id/book', function(req, res, next) {
 	models.Space.findOne({_id: req.params.id}, function(err, space) {
+		if (!space) {
+			err = 'Space missing';
+		}
 		if (err) {
 			console.log(err);
 			next(err);
